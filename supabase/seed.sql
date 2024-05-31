af866bd75ad667e3fb55c5aca6366822ed33811d
Tesario <vtesar2003@gmail.com>
2024-05-31 22:04:10 +0200

    Initial commit

---
+++ supabase/seed.sql
@@ -0,0 +1,15 @@
+insert into "Restaurant"
+  (name, url)
+values
+  ('Na Solnici', 'https://www.menicka.cz/7781-na-solnici.html'),
+  ('Suchi', 'https://suchibrno.cz/obedove-menu/'),
+  ('Pivní stáj', 'https://www.pivnistaj.cz/o-nas/'),
+  ('Jean Paul', 'https://www.menicka.cz/3884-jean-pauls-.html'),
+  ('Cosmopolis Grill', 'https://www.facebook.com/cosmopolisgrillBRNO'),
+  ('Parodie', 'https://wolt.com/cs/cze/brno/restaurant/parodie'),
+  ('Bastardo', 'https://restaurantguru.com/Bistro-Bastardo-Brno/menu'),
+  ('Garden Food Concept', 'https://www.gardenfoodconcept.cz/poledni-menu/'),
+  ('Špageta', 'https://spageta-bistro.business.site/'),
+  ('Mulgogi', 'https://mulgogi.cz/section:poledni-menu'),
+  ('Pho-Eden', 'https://www.facebook.com/profile.php?id=100063774180661'),
+  ('Kaliforňan', 'https://wolt.com/cs/cze/brno/restaurant/kalifornan-brno');
