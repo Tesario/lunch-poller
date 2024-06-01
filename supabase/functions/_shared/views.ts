1c9ec1b611370d1678abea1a73a8aa31f2769fa9
Tesario <vtesar2003@gmail.com>
2024-06-01 14:11:53 +0200

    Resolving conflicts

--- supabase/functions/_shared/views.ts
+++ supabase/functions/_shared/views.ts
@@ -11,40 +11,145 @@ export const getModalView = (restaurants: any) => {
   ));
 
   return ({
-    "title": {
-      "type": "plain_text",
-      "text": "Enhance the poll",
-      "emoji": true,
+    title: {
+      type: "plain_text",
+      text: "Enhance the poll",
+      emoji: true,
     },
-    "submit": {
-      "type": "plain_text",
-      "text": "Submit",
-      "emoji": true,
+    submit: {
+      type: "plain_text",
+      text: "Submit",
+      emoji: true,
     },
-    "type": "modal",
-    "close": {
-      "type": "plain_text",
-      "text": "Cancel",
-      "emoji": true,
+    type: "modal",
+    close: {
+      type: "plain_text",
+      text: "Cancel",
+      emoji: true,
     },
-    "blocks": [
+    blocks: [
       {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "Pick an restaurant from the list",
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Pick an restaurant from the list",
         },
-        "accessory": {
-          "type": "static_select",
-          "placeholder": {
-            "type": "plain_text",
-            "text": "Select an item",
-            "emoji": true,
+        accessory: {
+          type: "static_select",
+          placeholder: {
+            type: "plain_text",
+            text: "Select an item",
+            emoji: true,
           },
-          "options": options,
-          "action_id": "static_select-action",
+          options: options,
+          action_id: "static_select-action",
         },
       },
     ],
   });
 };
+
+export const getPollViewMessage = (randomRestaurants: any)=>(
+  [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `${randomRestaurants[0].name} | <${randomRestaurants[0].url}|Menu :link:>`,
+      },
+      accessory: {
+        type: "button",
+        text: {
+          type: "plain_text",
+          emoji: true,
+          text: "Vote",
+        },
+        value: "vote_1",
+      },
+    },
+    {
+      type: "context",
+      elements: [
+        {
+          type: "plain_text",
+          emoji: true,
+          text: "No votes",
+        },
+      ],
+    },
+    {
+      type: "divider",
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `${randomRestaurants[1].name} | <${randomRestaurants[1].url}|Menu :link:>`,
+      },
+      accessory: {
+        type: "button",
+        text: {
+          type: "plain_text",
+          emoji: true,
+          text: "Vote",
+        },
+        value: "vote_2",
+      },
+    },
+    {
+      type: "context",
+      elements: [
+        {
+          type: "plain_text",
+          emoji: true,
+          text: "No votes",
+        },
+      ],
+    },
+    {
+      type: "divider",
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `${randomRestaurants[2].name} | <${randomRestaurants[2].url}|Menu :link:>`,
+      },
+      accessory: {
+        type: "button",
+        text: {
+          type: "plain_text",
+          emoji: true,
+          text: "Vote",
+        },
+        value: "vote_3",
+      },
+    },
+    {
+      type: "context",
+      elements: [
+        {
+          type: "mrkdwn",
+          text: "No votes",
+        },
+      ],
+    },
+    {
+      type: "divider",
+    },
+    {
+      type: "actions",
+      elements: [
+        {
+          type: "button",
+          text: {
+            type: "plain_text",
+            emoji: true,
+            text: "Add a suggestion",
+          },
+          value: "add_suggestion",
+        },
+      ],
+    },
+  ]
+)
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,15 +1,15 @@
 insert into "Restaurant"
   (name, url)
 values
-  ('Na Solnici', 'https://www.menicka.cz/7781-na-solnici.html'),
-  ('Suchi', 'https://suchibrno.cz/obedove-menu/'),
-  ('Pivní stáj', 'https://www.pivnistaj.cz/o-nas/'),
-  ('Jean Paul', 'https://www.menicka.cz/3884-jean-pauls-.html'),
-  ('Cosmopolis Grill', 'https://www.facebook.com/cosmopolisgrillBRNO'),
-  ('Parodie', 'https://wolt.com/cs/cze/brno/restaurant/parodie'),
-  ('Bastardo', 'https://restaurantguru.com/Bistro-Bastardo-Brno/menu'),
-  ('Garden Food Concept', 'https://www.gardenfoodconcept.cz/poledni-menu/'),
-  ('Špageta', 'https://spageta-bistro.business.site/'),
-  ('Mulgogi', 'https://mulgogi.cz/section:poledni-menu'),
-  ('Pho-Eden', 'https://www.facebook.com/profile.php?id=100063774180661'),
-  ('Kaliforňan', 'https://wolt.com/cs/cze/brno/restaurant/kalifornan-brno');
+  (':stew: Na Solnici', 'https://www.menicka.cz/7781-na-solnici.html'),
+  (':sushi: Suchi', 'https://suchibrno.cz/obedove-menu/'),
+  (':hamburger: Pivní stáj', 'https://www.pivnistaj.cz/o-nas/'),
+  (':spaghetti: Jean Paul', 'https://www.menicka.cz/3884-jean-pauls-.html'),
+  (':stuffed_flatbread: Cosmopolis Grill', 'https://www.facebook.com/cosmopolisgrillBRNO'),
+  (':hamburger: Parodie', 'https://wolt.com/cs/cze/brno/restaurant/parodie'),
+  (':hamburger: Bastardo', 'https://restaurantguru.com/Bistro-Bastardo-Brno/menu'),
+  (':stew:Garden Food Concept', 'https://www.gardenfoodconcept.cz/poledni-menu/'),
+  (':spaghetti: Špageta', 'https://spageta-bistro.business.site/'),
+  (':sushi: Mulgogi', 'https://mulgogi.cz/section:poledni-menu'),
+  (':sushi: Pho-Eden', 'https://www.facebook.com/profile.php?id=100063774180661'),
+  (':green_salad: Kaliforňan', 'https://wolt.com/cs/cze/brno/restaurant/kalifornan-brno');
